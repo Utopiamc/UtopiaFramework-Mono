dc5af17b03dcaf23a27bacc86be2f587da728238
OskarWiedeweg <oskarwiedeweg01@gmail.com>
2022-11-01 17:48:57 +0100

    Add type to BeanDefinition

--- apis/injector-api/src/main/java/de/utopiamc/framework/inject/beans/BeanDefinition.java
+++ apis/injector-api/src/main/java/de/utopiamc/framework/inject/beans/BeanDefinition.java
@@ -4,12 +4,15 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 import java.util.Set;
 
 public interface BeanDefinition {
 
     @NotNull
     Set<Annotation> getAnnotations();
+
+    Type getType();
 
     @Nullable
     BeanDefinition getParent();
